f6e476cd19cdd99e9389c333647f85690ad197cb
Dmitriy <albatrozzz1@gmail.com>
2022-08-16 15:47:50 +0300

    fix

--- js/paws.js
+++ js/paws.js
@@ -1,16 +1,15 @@
 (() => {
     const paws = document.querySelector('.give-four-icons-wrap')
     const button = document.querySelector('.give-four-button')
+    const text = document.querySelector('.give-four-button-text')
+    const icon = document.querySelector('.give-four-button-icon')
 
-    button.addEventListener('mouseover', showPaws)
-    button.addEventListener('mouseout', hidePaws)
+    button.addEventListener('click', showPaws)
 
     function showPaws() {
         paws.style.display = "block"
+        button.style.backgroundColor = "#1C1919"
+        text.style.color = "#F6B626"
+        icon.style.fill = "#F6B626"
     }
-
-    function hidePaws() {
-        paws.style.display = "none"
-    }
-
 })()
